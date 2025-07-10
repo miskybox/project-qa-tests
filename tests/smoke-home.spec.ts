e8b39c240f83c92ed55313ca02fd4b13732c4d0b
Karisha:) <krshmelendez@gmail.com>
2025-07-11 00:10:54 +0200

    FEAT-smoke-home-test

--- tests/smoke-home.spec.ts
+++ tests/smoke-home.spec.ts
@@ -2,10 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('@smoke Verifica que la tienda carga y muestra productos', async ({ page }) => {
   await page.goto('https://shop.polymer-project.org/');
+  await page.waitForLoadState('networkidle');
 
-  await expect(page.locator('shop-home')).toBeVisible();
 
- 
+  await expect(page.locator('text=SHOP').first()).toBeVisible();
+
+
   const mensOuterwearLink = page.getByRole('link', { name: "Men's Outerwear Shop Now" });
   await expect(mensOuterwearLink).toBeVisible();
+
+  const cartLink = page.locator('a[href="/cart"]');
+  await expect(cartLink).toBeVisible();
 });
